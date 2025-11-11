2de671d10327d4715e39da1a6cfaacad5a5383d4
vowzss <vowzss@noideaindustry.com>
2025-11-11 19:50:17 +0000

    init: project files + code formatting

---
+++ xmake.lua
@@ -0,0 +1,46 @@
+-- Project settings
+set_project("jollet")
+set_version("1.0.0")
+set_languages("cxx17")
+
+-- Build modes
+add_rules("mode.debug", "mode.release")
+
+-- Dependencies
+add_requires("conan::spdlog/1.16.0", {alias = "spdlog"})
+add_requires("conan::utfcpp/4.0.8",  {alias = "utfcpp"})
+add_requires("conan::doctest/2.4.12", {alias = "doctest"})
+
+-- Library project
+target("jollet")
+    set_kind("headeronly")
+    set_basename("jollet")
+
+    -- Headers files
+    add_includedirs("include", {public = true})
+    add_headerfiles("include/(**.h)")
+    
+    -- Link dependencies
+    add_packages("spdlog", "utfcpp")
+    
+    -- Build modes
+    if is_mode("debug") then
+        set_symbols("debug")
+        set_optimize("none")
+    else
+        set_symbols("hidden")
+        set_optimize("fastest")
+    end
+target_end()
+
+-- Tests project
+target("jollet_tests")
+    set_kind("binary")
+    add_deps("jollet")
+
+    add_files("tests/**.cpp")
+
+    add_packages("spdlog", "utfcpp", "doctest")
+
+    set_rundir("$(projectdir)/tests")
+target_end()
